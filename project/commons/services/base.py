dfa388aadc1586a14a0e04bfb297a2d967e5614b
hiraq <hrxwan@gmail.com>
2018-02-18 22:26:56 +0700

    Adding new doc comments on commons.services.base

--- project/commons/services/base.py
+++ project/commons/services/base.py
@@ -1,11 +1,51 @@
 from abc import ABC, abstractmethod
 
 class BaseService(ABC):
+    """
+    I'm using ServiceObject pattern like in Rails.
+    With this pattern, we can moving all activities
+    in controller to their services.  So we can
+    accept a service as controller extensions.
+
+    A service should has a dependencies injections
+    maybe like form validations, a repository or a model
+    or any classes or functions, and we should test all
+    activities with or without their mocks.
+
+    A service should have only one main activity, like:
+    - registering new member -> one service
+    - creating a shopping cart -> one service
+    - etc
+
+    We can chaining abstract methods like this:
+
+    serviceA.validate().call()
+
+    Depends on your needs.
+
+    By default there are only two abstract methods:
+    1. validate - anything relate to data validations
+    2. call - used to run this main service function.
+    """
 
     @abstractmethod
     def validate(self):
+        """
+        Returns:
+            anything you needs
+
+        Raises:
+            commons.exceptions.ServiceValidationError
+        """
         pass
 
     @abstractmethod
     def call(self):
+        """
+        Returns:
+            anything you needs
+
+        Raises:
+            commons.exceptions.ServiceCallerError
+        """
         pass
