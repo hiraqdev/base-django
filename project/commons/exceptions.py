5eebcec63bfe7b380b29f4050f1ba7149f18e89c
hiraq <hrxwan@gmail.com>
2018-02-18 22:09:13 +0700

    Create base exceptions

---
+++ project/commons/exceptions.py
@@ -0,0 +1,12 @@
+class BaseException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+class ServiceValidationError(BaseException):
+    pass
+
+class ServiceCallerError(BaseException):
+    pass
