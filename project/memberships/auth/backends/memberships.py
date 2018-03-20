33998acc1471faeb3226469fcc955b37faf88e06
hiraq <hrxwan@gmail.com>
2018-03-20 10:38:50 +0700

    Add filter for user activation and request reset pass

--- project/memberships/auth/backends/memberships.py
+++ project/memberships/auth/backends/memberships.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
 
+from memberships.models import Member
+
 class Memberships(object):
 
     def authenticate(self, request, email=None, password=None):
@@ -8,6 +10,13 @@ class Memberships(object):
             user = User.objects.get(email=email)
             pwd_valid = check_password(password, user.password)
             if not pwd_valid:
+                return None
+
+            if not user.is_active:
+                return None
+
+            member = Member.objects.get(user=user)
+            if member.request_to_reset_pass:
                 return None
 
             return user
